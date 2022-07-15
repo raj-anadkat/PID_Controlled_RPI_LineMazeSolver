65c831f7d4a7a659209b1df8fc6013ce22b62080
raj-anadkat <109377585+raj-anadkat@users.noreply.github.com>
2022-07-16 01:15:46 +0530

    Add files via upload

---
+++ Basic_PID_Line_Follower.py
@@ -0,0 +1,149 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+Ena,In1,In2 = 26,27,22
+Enb,In3,In4 = 4,23,24
+
+S1,S2,S3,S4,S5 = 5,6,13,12,25
+
+##MotorSetup
+GPIO.setup(Ena,GPIO.OUT)
+GPIO.setup(Enb,GPIO.OUT)
+GPIO.setup(In1,GPIO.OUT)
+GPIO.setup(In2,GPIO.OUT)
+GPIO.setup(In3,GPIO.OUT)
+GPIO.setup(In4,GPIO.OUT)
+
+##PWMsetup
+pwm1 = GPIO.PWM(Ena,200)
+pwm2 = GPIO.PWM(Enb,200)
+pwm1.start(100)
+pwm2.start(100)
+
+##SensorSetup
+GPIO.setup(S1,GPIO.IN)
+GPIO.setup(S2,GPIO.IN)
+GPIO.setup(S3,GPIO.IN)
+GPIO.setup(S4,GPIO.IN)
+GPIO.setup(S5,GPIO.IN)
+
+##constants
+error = 0
+Kp,Ki,Kd = 6,0,1
+P,I,D = 0,0,0
+previous_error,previous_I = 0,0
+
+## initializeSpeed
+initial_motor_speed = 30
+
+
+sensor = [0,0,0,0,0]
+
+def read_sensor_values():
+    
+    global error
+    sensor[0] = GPIO.input(S1)
+    sensor[1] = GPIO.input(S2)
+    sensor[2] = GPIO.input(S3)
+    sensor[3] = GPIO.input(S4)
+    sensor[4] = GPIO.input(S5)
+    
+    
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 1) and (sensor[3] == 1) and (sensor[4] == 0)):
+        #11110
+        error = 4
+    
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 1) and (sensor[3] == 0) and (sensor[4] == 0)):
+        #11100
+        error = 3
+        
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 1) and (sensor[3] == 0) and (sensor[4] == 1)):
+        #11101
+        error = 2
+    
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 0) and (sensor[3] == 0) and (sensor[4] == 1)):
+        #11001
+        error = 1
+        
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 0) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #11011
+        error = 0
+        
+    if ((sensor[0] == 1) and (sensor[1] == 0) and (sensor[2] == 0) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #10011
+        error = -1
+        
+    if ((sensor[0] == 1) and (sensor[1] == 0) and (sensor[2] == 1) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #10111
+        error = -2
+        
+    if ((sensor[0] == 0) and (sensor[1] == 0) and (sensor[2] == 1) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #00111
+        error = -3
+        
+    if ((sensor[0] == 0) and (sensor[1] == 1) and (sensor[2] == 1) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #01111
+        error = -4
+        
+    if ((sensor[0] == 1) and (sensor[1] == 1) and (sensor[2] == 1) and (sensor[3] == 1) and (sensor[4] == 1)):
+        #11111
+        if (error == -4):
+            error = -5
+        else:
+            error = 5
+            
+    print(sensor)
+    print(error)
+    print("...........")
+    
+    
+    return error
+
+def constrain(val, min_val, max_val):
+
+    if val < min_val: 
+        val = min_val
+
+    elif val > max_val: 
+        val = max_val
+
+    return val
+    
+    
+def calculate_pid():
+    global I
+    global previous_error
+    P = error
+    I = I + previous_error
+    D = error - previous_error
+    
+    PID_value = (Kp*P) + (Ki*I) + (Kd*D)
+    previous_I = I
+    previous_error = error
+    return PID_value
+    
+def motor_control():
+    left_motor_speed = initial_motor_speed-PID_value
+    right_motor_speed = initial_motor_speed+PID_value
+    
+    left_motor_speed = constrain(left_motor_speed,0,100)
+    right_motor_speed = constrain(right_motor_speed,0,100)
+    
+    pwm2.ChangeDutyCycle(left_motor_speed)
+    pwm1.ChangeDutyCycle(right_motor_speed)
+    GPIO.output(In1,GPIO.HIGH)
+    GPIO.output(In2,GPIO.LOW)
+    GPIO.output(In3,GPIO.LOW)
+    GPIO.output(In4,GPIO.HIGH)
+    
+    
+    
+    
+      
+while 1:
+    read_sensor_values()
+    PID_value=calculate_pid()
+    motor_control()
